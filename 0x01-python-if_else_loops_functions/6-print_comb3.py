1b4b9b6a0075e51319dcc171473d891f26bf8a15
a-idk <idoko_research@outlook.com>
2023-10-03 06:58:24 -0700

    2-digit combo

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# printing all possible combination of 2-digits
+# from 0 to 10
+
+for f_dig in range(0, 10):
+    for s_dig in range(f_dig + 1, 10):
+        if f_dig != 8 and s_dig != 9:
+            print("{}{}".format(f_dig, s_dig), end=", ")
+        else:
+            print("{}{}".format(f_dig, s_dig))
