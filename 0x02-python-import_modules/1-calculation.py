85c386dfe96db65b051073fccde912d24ecb0f37
a-idk <idoko_research@outlook.com>
2023-10-05 06:38:54 -0700

    calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
 
-# @a_idk scripting
-
-'''program that imports functions from the file calculator_1.py,
-does some Maths, and prints the result.'''
-
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
-    # defining and initializing variables
     a = 10
     b = 5
 
-    # mathematical computations
     summer = add(a, b)
     subtracter = sub(a, b)
     multiplier = mul(a, b)
     divider = div(a, b)
 
-    # output displays
     print(f"{a} + {b} = {summer}")
     print(f"{a} - {b} = {subtracter}")
     print(f"{a} * {b} = {multiplier}")
