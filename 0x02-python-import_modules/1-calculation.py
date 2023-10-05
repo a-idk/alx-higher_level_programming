dff51a4411bb28519c3a55e55ae0c7a917a924b4
a-idk <idoko_research@outlook.com>
2023-10-05 06:47:36 -0700

    checking the checker response

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 
+# @a_idk scripting
+
+''' Program that imports functions from the file calculator_1.py,
+does some Maths, and prints the result.'''
+
 if __name__ == "__main__":
-    """Print the sum, difference, multiple and quotient of 10 and 5."""
     from calculator_1 import add, sub, mul, div
 
+    # defining my variables
     a = 10
     b = 5
 
+    # outputting the maths
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
