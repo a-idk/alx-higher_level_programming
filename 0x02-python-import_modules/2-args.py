15e2d62ca0d8b1e4cd0f747f4a9336a0de0b9e2d
a-idk <idoko_research@outlook.com>
2023-10-05 07:30:25 -0700

    script dynamic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
 # @a_idk scripting
-import string
-print(string.ascii_uppercase)
+
+if __name__ == "__main__":
+    
+    import sys
+
+    # counting the number of arguments
+    num = len(sys.argv) - 1
+    
+    # conditions
+    if num == 0:
+        print("0 arguments.")
+    elif num > 1:
+        print("{} arguments:".format(num))
+    else:
+        print("1 argument:")
+
+    # printing the respective arguments
+    for x in range(num):
+        print(f"{x + 1}: {sys.argv[x + 1]}")
+        # print("{}: {}".format(x + 1, sys.argv[x + 1]))
