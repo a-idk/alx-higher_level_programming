dab5d5c8923bf03903cbcf607820c764eadb1125
a-idk <idoko_research@outlook.com>
2023-10-05 05:33:43 -0700

    import add

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#@a_idk scripting
+
+if __name__ == "__main__":
+    from add_0 import add
+
+
+    a, b = 1, 2
+    # summing and printing the sum of a + b
+    print("{} + {} = {}".format(a, b, add(a, b)))
