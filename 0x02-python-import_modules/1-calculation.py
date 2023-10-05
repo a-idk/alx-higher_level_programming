a2ea440df2b4d0d58c6b84676f1f1c430c6bbfd6
a-idk <idoko_research@outlook.com>
2023-10-05 07:11:08 -0700

    calculate

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,9 +2,6 @@
 # @a_idk scripting
 
 
-""" program that imports functions from the file calculator_1.py,
-does some Maths, and prints the result."""
-
 if __name__ == "__main__":
 
     from calculator_1 import add, sub, mul, div
