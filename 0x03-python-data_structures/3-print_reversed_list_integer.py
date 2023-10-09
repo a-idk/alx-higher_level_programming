835f7e2e0d789b31c004cbff9c0a126d1c49537f
a-idk <idoko_research@outlook.com>
2023-10-09 04:22:05 -0700

    print reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,5 +5,8 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    for idx in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[idx]))
+    my_list.reverse()
+#    for idx in range(len(my_list) - 1, -1, -1):
+#        print("{:d}".format(my_list[idx]))
+    for idx in my_list:
+        print("{:d}".format(idx))
