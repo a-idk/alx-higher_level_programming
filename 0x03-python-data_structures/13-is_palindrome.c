dea555f1fd598adb86809886dd5ea21e6074f876
a-idk <idoko_research@outlook.com>
2023-10-09 05:33:32 -0700

    update palind

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
-/* listint_t *reverse_listint(listint_t **head);*/
-/* int is_palindrome(listint_t **head);*/
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
 
 /**
  * reverse_listint - Function that reverses a
