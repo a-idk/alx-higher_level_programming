af34e2721d7e0e936e764ec5f1b67616f9b09d38
a-idk <idoko_research@outlook.com>
2023-10-11 03:47:50 -0700

    replace/add key/value

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# @a_idk scripting
+
+# function that replaces or adds key/value in a dictionary
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return (a_dictionary)
