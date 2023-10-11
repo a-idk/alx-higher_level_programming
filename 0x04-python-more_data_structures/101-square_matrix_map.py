c161cdf45cd5e8fd94d8b44fe67cc806aa59566b
a-idk <idoko_research@outlook.com>
2023-10-11 12:20:08 -0700

    task 14 update

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-# @a_idk scripting
-
-# function that computes the square value of all integers of a
-# matrix using map
-
-
-def square_matrix_map(matrix=[]):
+def square_matrix_map(matrix=[]):  # function that del keys
     return (list(map(lambda i: list(map(lambda j: j**2, i)), matrix)))
