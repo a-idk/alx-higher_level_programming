cb4dc4d72267666ecf64c747afb67b3168e8b99f
a-idk <idoko_research@outlook.com>
2023-10-11 12:06:15 -0700

    task 5 update

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-# @a_idk scripting
-
-# function that returns the number of keys in a dictionary
-
-
 def number_keys(a_dictionary):
-    # initialing variables and setting the list
-    keys_lst = list(a_dictionary.keys())
-    keys_num = 0
-    # counting the keys
-    for x in key_lst:
-        keys_num = keys_num + 1
-    return (keys_num)
+    num = 0
+    list_keys = list(a_dictionary.keys())
+
+    for i in list_keys:
+        num += 1
+
+    return (num)
---
+++ 0x04-python-more_data_structures/5-number_keys2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# @a_idk scripting
+
+# function that returns the number of keys in a dictionary
+
+
+def number_keys(a_dictionary):
+    # initialing variables and setting the list
+    keys_lst = list(a_dictionary.keys())
+    keys_num = 0
+    # counting the keys
+    for x in key_lst:
+        keys_num = keys_num + 1
+    return (keys_num)
