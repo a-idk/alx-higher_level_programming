718c8a699ad6ffd704cf37bb27e1b002928c857d
a-idk <idoko_research@outlook.com>
2023-10-11 12:13:25 -0700

    task 5 update

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -9,6 +9,7 @@ def number_keys(a_dictionary):
     keys_lst = list(a_dictionary.keys())
     key_num = 0
 
+    # counting the keys
     for x in keys_lst:
         key_num = key_num + 1
     return (key_num)
