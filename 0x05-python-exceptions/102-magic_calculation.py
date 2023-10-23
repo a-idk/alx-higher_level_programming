cda411d678eb801967922db7834c82191b1d205b
a-idk <idoko_research@outlook.com>
2023-10-23 11:25:39 -0700

    advanced magic calc

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -12,9 +12,9 @@ def magic_calculation(a, b):
     outcome = 0
     for x in range(1, 3):
         try:
-            if x > a:
+            if a < x:
                 raise Exception('Too far')
-            outcome = outcome + a ** b / x
+            outcome = outcome + (a ** b) / x
         except Exception:
             outcome = a + b
             break
