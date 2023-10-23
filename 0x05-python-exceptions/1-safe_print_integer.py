443321ed93ac472449964eca489ef81e1887d949
a-idk <idoko_research@outlook.com>
2023-10-23 04:32:23 -0700

    safe int printing update

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -12,7 +12,7 @@ def safe_print_integer(value):
         print("{:d}".format(get_input))  # Required print format
         # print()  # Newline print
         return (True)
-    except (TypeError):
-        return (False)
-    except (TypeError):
+#    except (TypeError):
+#        return (False)
+    except (ValueError):
         return (False)
