4f9e0ae406e16c73a7ceac9937dd9bc8276e362f
a-idk <idoko_research@outlook.com>
2023-10-23 08:07:54 -0700

    dividing lists update

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,33 +11,19 @@ def list_division(my_list_1, my_list_2, list_length):
     for idx in range(list_length):
         try:
             # Acquiring the elements from the lists
-            x = my_list_1[idx] if idx < len(my_list_1) else 0
-            y = my_list_2[idx] if idx < len(my_list_2) else 0
-            # type check
-            if not isinstance(x, (int, float)) or \
-               not isinstance(y, (int, float)):
-                raise TypeError("wrong type")
-            if y == 0:  # division by zero check
-                raise ZeroDivisionError("division by 0")
-            # appending subsequent outcomes/results
-            division = x / y
-            if division.is_integer():
-                outcome.append(float(division))
-            else:
-                outcome.append(division)
-        # handling and printing exceptions
-        except TypeError as exception:
-            print(exception)
+            outcome.append(my_list_1[idx] / my_list_2[idx])
+        except ZeroDivisionError:
             outcome.append(0)
-        except ZeroDivisionError as exception:
-            print(exception)
+            print('division by 0')
+            continue
+        except TypeError:
             outcome.append(0)
-#        except IndexError:
-#            print("out of range")
-#            outcome.append(0.0)
-    if list_length > max(len(my_list_1), len(my_list_2)):
-        outcome.append(0.0)
-    if 0.0 in outcome:
-        print("out of range")
-#        outcome.append(0.0)
+            print('wrong type')
+            continue
+        except IndexError:
+            outcome.append(0)
+            print('out of range')
+            continue
+        finally:
+            pass
     return outcome
