29005c363d1c4fde605dd67daba1f0005970e71c
a-idk <idoko_research@outlook.com>
2023-10-25 19:46:44 -0700

    advanced task update

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -19,10 +19,15 @@ class MagicClass:
         else:
             self.__radius = radius
 
-    def circumference(self):
+'''    def circumference(self):
         """ calculates circumference of circle """
         return (math.pi * self.__radius * 2)
+'''
     
     def area(self):
         """ Calculates Area of Circle """
         return (math.pi * self.__radius ** 2)
+
+    def circumference(self):
+        """ calculates circumference of circle """
+        return (math.pi * self.__radius * 2)
