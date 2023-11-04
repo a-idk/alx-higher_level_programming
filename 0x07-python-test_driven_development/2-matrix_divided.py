847960465b86d2196905acc19e1fc56d3978ac21
a-idk <idoko_research@outlook.com>
2023-11-03 20:19:28 -0700

    task 2 update

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -28,13 +28,16 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
 
     if len(matrix) is 0:
-        raise TypeError("matrix must be a \
-                matrix (list of lists) of integers/floats")
+        raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats"
+                )
     elif len(matrix) is 1:
         for x in matrix[0]:
             if type(x) not in [int, float]:
-                raise TypeError("matrix must be a \
-                        matrix (list of lists) of integers/floats")
+                raise TypeError(
+                        """matrix must be a matrix (list of lists) of
+                        integers/floats"""
+                        )
             tmp.append(float("{:.2f}".format(x / div)))
         matrix_new.append(tmp)
         return matrix_new
@@ -42,15 +45,20 @@ def matrix_divided(matrix, div):
         length = len(matrix[0])
         for i in matrix:
             if not isinstance(i, list):
-                raise TypeError("matrix must be a \
-                        matrix (list of lists) of integers/floats")
+                raise TypeError(
+                        """matrix must be a matrix (list of lists) of
+                        integers/floats"""
+                        )
             if len(i) != length:
-                raise TypeError("Each row of the matrix \
-                        must have the same size")
+                raise TypeError(
+                        "Each row of the matrix must have the same size"
+                        )
             for n in i:
                 if type(n) not in [int, float]:
-                    raise TypeError("matrix must be a \
-                            matrix (list of lists) of integers/floats")
+                    raise TypeError(
+                            """matrix must be a matrix (list of lists)
+                            of integers/floats"""
+                            )
                 tmp.append(float("{:.2f}".format(n / div)))
             matrix_new.append(tmp)
             tmp = []
