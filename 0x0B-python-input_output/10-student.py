58b16e15316cb0447f3ab037647c2bd4253c4d8f
a-idk <idoko_research@outlook.com>
2023-11-07 15:11:43 -0800

    python i/o

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -13,16 +13,16 @@ class Student:
     """
 
     def __init__(self, first_name, last_name, age):
-    """
-    initializing the class Student
-    Args:
-        first_name: first name of student
-        last_name: last name of student
-        age: age of student
-    """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+        """
+        initializing the class Student
+        Args:
+            first_name: first name of student
+            last_name: last name of student
+            age: age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
         """
