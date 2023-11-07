61cd3cd604ed0972691c6dc178d29c92402b23af
a-idk <idoko_research@outlook.com>
2023-11-06 22:10:00 -0800

    task 1 update

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -17,3 +17,8 @@ class MyList(list):
         (Ascending order)
         """
         print(sorted(self))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/1-my_list.txt")
