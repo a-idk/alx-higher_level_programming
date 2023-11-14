d35ac1cdf53a5ef0c0a130eb6b343777b54eeaa8
a-idk <idoko_research@outlook.com>
2023-11-14 14:54:26 -0800

    models update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -39,7 +39,7 @@ class Base:
         if not list_dictionaries:
             return ("[]")
         else:
-            return dumps(list_dictionaries)
+            return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
