a5013dbf5c06462491c4e70b2a4c070579059d50
a-idk <idoko_research@outlook.com>
2024-01-09 11:21:45 +0100

    tasks 6 review

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const lst = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const lst = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let x = 0; x < lst.length; x = x + 1) {
   console.log(lst[x]);
