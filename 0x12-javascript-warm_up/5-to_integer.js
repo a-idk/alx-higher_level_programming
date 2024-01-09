0ef52a3b6f583eb3044d32a73971d66de50f021b
a-idk <idoko_research@outlook.com>
2024-01-09 02:08:54 +0100

    all other tasks

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,8 +4,8 @@
 const arg = process.argv;
 const n = parseInt(argv[2]);
 
-if (isNaN(n) {
-	  console.log('Not a number');
+if (isNaN(n)) {
+  console.log('Not a number');
 } else {
-	  console.log(`My number: ${n}`);
+  console.log(`My number: ${n}`);
 }
