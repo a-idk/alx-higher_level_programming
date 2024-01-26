a7993b690412e689b26fde61e5bb4ba762bb14fd
a-idk <idoko_research@outlook.com>
2024-01-26 23:40:07 +0100

    status check

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,16 +4,17 @@ Title: What's my Status?
 Description: A script that fetches "https://alx-intranet.hbtn.io/status"
 Author: @a_idk
 """
-from urllib import request
+# from urllib import request
+import urllib.request
 
 
 if __name__ == "__main__":
     req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
 
-    with urllib.request.urlopen(req) as reponse:
-        content = response.read()
+    with urllib.request.urlopen(req) as response:
+        cont = response.read()
         # formating the print as requested
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+        print("\t- type: {}".format(type(cont)))
+        print("\t- content: {}".format(cont))
+        print("\t- utf8 content: {}".format(cont.decode("utf-8")))
