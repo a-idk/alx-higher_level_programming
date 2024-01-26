331e131cc298b96a560185dd3773c98a42d9dcad
a-idk <idoko_research@outlook.com>
2024-01-27 00:33:16 +0100

    error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,6 +6,7 @@ Description: A script that takes in a URL, sends a request to the URL and
 Author: @a_idk
 """
 # from urllib import request
+import urllib.error
 import urllib.request
 import sys
 # import urllib.parse
