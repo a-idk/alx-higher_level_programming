95fd792ef9e24b3d417b5f452f98b10e60e3a7a9
a-idk <idoko_research@outlook.com>
2024-01-26 02:35:53 +0100

    find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,10 +7,24 @@ def find_peak(list_of_integers):
     """
     find a peak Module
     """
-    if (len(list_of_integers) == 0 or (not list_of_integers)):
+    if (not list_of_integers) or len(list_of_integers) == 0:
         return None
-
-    if list_of_integers:
-        list_of_integers.sort(reverse=True)
+    if len(list_of_integers) == 1:
         result = list_of_integers[0]
         return result
+    if len(list_of_integers) == 2:
+        result = max(list_of_integers)
+        return result
+
+    num = len(list_of_integers)
+    centre = int(num / 2)
+    high_num = list_of_integers[centre]
+    pos = centre + 1
+    neg = centre - 1
+
+    if high_num > list_of_integers[neg] and high_num > list_of_integers[pos]:
+        return high_num
+    elif high_num < list_of_integers[neg]:
+        return find_peak(list_of_integers[:centre])
+    else:
+        return find_peak(list_of_integers[pos:])
