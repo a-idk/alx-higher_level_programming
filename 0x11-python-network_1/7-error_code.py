445ea0f5b773381c7c4f04d8a9a9af1be29c0938
a-idk <idoko_research@outlook.com>
2024-01-27 01:07:53 +0100

    error code (requests)

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Title: Error code (requests)
+Description: A script that takes in a URL and displays the body of the response.
+    Also gives error code if http encounters error.
+Author: @a_idk
+"""
+# from urllib import request
+# import urllib.error
+# import urllib.request
+import sys
+# import urllib.parse
+import requests
+
+
+if __name__ == "__main__":
+    url_given = sys.argv[1]
+    # email_given = sys.argv[2]
+    # encoding the email
+    content = requests.get(url_given)
+
+    if content.status_code < 400:
+        print(content.text)
+    else:
+        print(f'Error code: {content.status_code}')
