29d55f57b6251374741a5bb3b164c0f8cafcffe5
a-idk <idoko_research@outlook.com>
2024-02-20 17:00:30 +0100

    task 0 fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,13 +2,12 @@
 
 const filesys = require('fs'); // import file system module
 // process.argv[0] - executable node.js, ~argv[1] - script executed
-const file_path = process.argv[2]; // get file name from command line
+const filePath = process.argv[2]; // get file name from command line
 
-filesys.readFile(file_path, 'utf-8', function (error, data) {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log(data);
-	}
-	
+filesys.readFile(filePath, 'utf-8', function (error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
 });
